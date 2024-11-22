41e173d0a89c24410f9157790b103fa082c26f4c
juanm <juanm@beto>
2024-11-22 12:01:06 +0100

    SIN INTERFAZ

--- BatallaNaval/src/bn/JuegoBatallaNaval.java
+++ BatallaNaval/src/bn/JuegoBatallaNaval.java
@@ -30,13 +30,7 @@ public class JuegoBatallaNaval {
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(3, 1, 10, 10)); // Diseño de rejilla (3 filas, 1 columna, con espacios).
  
-        // Botón para jugar contra la computadora.
-        JButton jugarContraComputadoraButton = new JButton("Jugar contra la computadora");
-        jugarContraComputadoraButton.addActionListener(e -> {
-            frame.dispose(); // Cierra el menú principal.
-            iniciarJuegoContraComputadora(); // Llama al método para iniciar el modo de juego contra la computadora.
-        });
- 
+
         // Botón para iniciar el servidor multijugador (primer jugador).
         JButton primerJugadorMultijugadorButton = new JButton("Modo multijugador (iniciar servidor)");
         primerJugadorMultijugadorButton.addActionListener(e -> {
@@ -60,7 +54,6 @@ public class JuegoBatallaNaval {
         });
  
         // Añade los botones al panel.
-        buttonPanel.add(jugarContraComputadoraButton);
         buttonPanel.add(primerJugadorMultijugadorButton);
         buttonPanel.add(segundoJugadorMultijugadorButton);
  
@@ -69,10 +62,6 @@ public class JuegoBatallaNaval {
         frame.setVisible(true); // Muestra la ventana al usuario.
     }
  
-    // Método para iniciar el modo de juego contra la computadora.
-    private void iniciarJuegoContraComputadora() {
-        javax.swing.SwingUtilities.invokeLater(() -> new BatallaNavalInterfa()); // Llama a otra clase que manejará este modo de juego.
-    }
  
     // Método para iniciar el servidor multijugador y manejar al primer jugador.
     private void iniciarServidorYPrimerJugadorMultijugador() throws IOException {
